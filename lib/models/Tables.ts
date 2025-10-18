36cb2bdbac20de2e37191ca19b1ea5c6c10edbce
alxjrvs@gmail.com <alxjrvs@gmail.com>
2025-10-18 14:10:20 -0400

    Cleanup Tables

--- lib/models/Tables.ts
+++ lib/models/Tables.ts
@@ -1,12 +1,10 @@
 import { BaseModel } from "../BaseModel.js";
-import { Table, SalvageUnionTables } from "../types/inferred.js";
+import { Table } from "../types/inferred.js";
 import tablesData from "../../data/tables.json" with { type: "json" };
 import tablesSchema from "../../schemas/tables.schema.json" with { type: "json" };
-
 
 export class TablesModel extends BaseModel<Table> {
   constructor() {
     super(tablesData as Table[], tablesSchema);
   }
 }
-
